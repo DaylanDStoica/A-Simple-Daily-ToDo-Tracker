064478bba970e36a3806a6e925b44791cecd3ec1
DaylanDStoica <daylanstoica@gmail.com>
2025-08-29 13:39:44 -0700

    added some code to main_functions.js, require testing

--- main_functions.js
+++ main_functions.js
@@ -3,14 +3,43 @@
 
 const PERMANENT_RECORD = "dailytasklist.txt";
 const TEMPORARY_RECORD = "todaystasklist.txt";
+
+function readTxtFile(filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(text => console.log(text))
+        // outputs the content of the text file
+}
+
+function writeToTxtFile(filePath, content){
+    fetch(filePath, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: content
+    })
+}
+
 // at the start of the day, load the tasks from the file
-function loadTasks() {
+// TEMPORARY_RECORD.file_contents = PERMANENT_RECORD.file_contents
+function resetTasks() {
+    // code to reset today's tasks
     // code to read tasks from dailytasklist.txt into todaystasklist.txt
+    // task_list = TEMPORARY_RECORD.file_contents;
+    temp_contents = readTxtFile(PERMANENT_RECORD);
+    writeToTxtFile(TEMPORARY_RECORD, temp_contents);
+}
+
+function loadTasks() {
+    // code to load today's tasks from todaystasklist.txt
+    return readTxtFile(TEMPORARY_RECORD);
 }
 
 // when a new task is added, save it to the file
-function saveTask(task) {
+function addTask(task) {
     // code to append the new task to dailytasklist.txt
+    writeToTxtFile(PERMANENT_RECORD, task);
 }
 
 function markTaskProgress(taskId, units) {
